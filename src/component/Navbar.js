d0ecafefec1b6b9e381f81af7cbc659d2f13da04
ahmadagayev <ehmed.agayev63@gmail.com>
2022-04-28 15:52:16 +0400

    Compileted

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='header'>
-      <Link to="/">CryptoHunter</Link>
+      <Link to="/cryptohunter">CryptoHunter</Link>
         <ul className='navbar'>
             <li className='navbar-item'><Link to="/">Ana Səhifə</Link></li>
             <li className='navbar-item'><Link to="/trends">Trendlər</Link></li>
